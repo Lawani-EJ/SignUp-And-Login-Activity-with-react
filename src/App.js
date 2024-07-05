7e37422889f731b2f066fba78eac073f96f73c2f
Lawani-EJ <elyonjohn60@gmail.com>
2024-07-05 12:27:44 +0100

    Added useform and update login form with the name input and validations.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useForm } from "react-hook-form";
+import Login from './components/Login';
 
 function App() {
+  const {register, handleSubmit, formState:{errors}}  = useForm();
+  const onSubmit = (data) => console.log(data)
   return (
     <>
     <p className="title">Registration Form</p>
-    <form className='App'>
-      <input type="text"/>
-      <input type="email"/>
-      <input type="password"/>
-      <input type={"submit"}/>
-      style={{backgroundColor: "red"}}
+    <form className='App' onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" {...register("name")}/>
+      <input type="email" {...register("email", {required: true})}/>
+      {errors.email && <span style={{color: "red"}}>Email Is Mandatory!</span>}
+      <input type="password" {...register("Password")}/>
+      <input type={"submit"} style={{backgroundColor: "red"}} />
     </form>
+
+    <Login/>
     </>
   );
 }
