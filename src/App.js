abedf368da040a385aeabc75c6225d617eec076a
Lawani-EJ <elyonjohn60@gmail.com>
2024-07-06 21:43:38 +0100

    Commented out the previous registration form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 // import logo from './logo.svg';
 import './App.css';
-import { useForm } from "react-hook-form";
+// import { useForm } from "react-hook-form";
 import Login from './components/Login';
 
 function App() {
-  const {register, handleSubmit, formState:{errors}}  = useForm();
-  const onSubmit = (data) => console.log(data)
+  // const {register, handleSubmit, formState:{errors}}  = useForm();
+  // const onSubmit = (data) => console.log(data)
   return (
-    <>
-    <p className="title">Registration Form</p>
-    <form className='App' onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" {...register("name")}/>
-      <input type="email" {...register("email", {required: true})}/>
-      {errors.email && <span style={{color: "red"}}>Email Is Mandatory!</span>}
-      <input type="password" {...register("Password")}/>
-      <input type={"submit"} style={{backgroundColor: "red"}} />
-    </form>
+    // <>
+    // <p className="title">Registration Form</p>
+    // <form className='App' onSubmit={handleSubmit(onSubmit)}>
+    //   <input type="text" {...register("name")}/>
+    //   <input type="email" {...register("email", {required: true})}/>
+    //   {errors.email && <span style={{color: "red"}}>Email Is Mandatory!</span>}
+    //   <input type="password" {...register("Password")}/>
+    //   <input type={"submit"} style={{backgroundColor: "red"}} />
+    // </form>
+
+    // <Login/>
+    // </>
 
     <Login/>
-    </>
   );
 }
 
